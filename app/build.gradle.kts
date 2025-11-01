212063fa0bbcd9f5551d6f45e4e1d82714097ed0
qlmio <microsoftedgeql@gmail.com>
2025-11-01 18:21:32 +0800

    - 优化 UI界面 - 优化 误转发 - 添加 查看源码卡片

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "io.github.chimio.inxlocker"
         minSdk = 29
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.3"
+        versionCode = 10
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
